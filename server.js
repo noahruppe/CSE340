aa0e8a5ddf84ab01232a401103216a693d1fca90
noahruppe <noah.ruppe30@gmail.com>
2024-10-04 17:52:51 -0400

    s

--- server.js
+++ server.js
@@ -32,35 +32,35 @@ app.use(static)
 // index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv",(inventoryRoute))
-// app.use(async(req,res,next,) =>{
-//   next({status: 404, message: 'Sorry we appear to have lost that page.'})
-// })
+app.use(async(req,res,next,) =>{
+  next({status: 404, message: 'Sorry we appear to have lost that page.'})
+})
 
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
 *************************/
-// app.use(async (err, req, res, next) => {
-//   let nav = await utilities.getNav();
-//   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
   
-//   let message; // Declare a variable for the message
+  let message; // Declare a variable for the message
 
-//   // Custom messages for different error types
-//   if (err.status === 404) {
-//       message = err.message; // Specific message for 404 errors
-//   } else if (err.status === 500) {
-//       message = 'Oh no! There was a server error. Please try again later.'; // Message for 500 errors
-//   } else {
-//       message = 'Oh no! There was a crash. Maybe try a different route?'; // General message for other errors
-//   }
+  // Custom messages for different error types
+  if (err.status === 404) {
+      message = err.message; // Specific message for 404 errors
+  } else if (err.status === 500) {
+      message = 'Oh no! There was a server error. Please try again later.'; // Message for 500 errors
+  } else {
+      message = 'Oh no! There was a crash. Maybe try a different route?'; // General message for other errors
+  }
 
-//   res.render("errors/error", {
-//       title: err.status || 'Server Error', // Use the status for the title
-//       message, // Use the determined message
-//       nav
-//   });
-// });
+  res.render("errors/error", {
+      title: err.status || 'Server Error', // Use the status for the title
+      message, // Use the determined message
+      nav
+  });
+});
 
 
 
