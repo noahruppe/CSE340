feed71572f7ec46682cf2e71dd95d1782082c8f3
noahruppe <noah.ruppe30@gmail.com>
2024-10-04 19:20:44 -0400

    s

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const utilities = require("../CSE340/utilities/index.js")
+const utilities = require("../CSE340/utilities/")
 
 /* ******************************************
  * This server.js file is the primary file of the 
