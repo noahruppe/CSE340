05acd0012b6f75ce70fa25f8f6b580a08558fd20
noahruppe <noah.ruppe30@gmail.com>
2024-10-04 23:36:00 -0400

    s

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-const utilities = require("../CSE340/utilities/");
 
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/index"); // Corrected path for utilities
 
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
-/* ***********************
- * Require Statements
- *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const Util = require("./utilities/index")
-
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 // index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
-app.use("/inv",(inventoryRoute))
-app.use(async(req,res,next,) =>{
-  next({status: 404, message: 'Sorry we appear to have lost that page.'})
-})
+app.get("/", utilities.handleErrors(baseController.buildHome));
+app.use("/inv", inventoryRoute);
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
 
 /* ***********************
 * Express Error Handler
@@ -63,22 +58,16 @@ app.use(async (err, req, res, next) => {
   });
 });
 
-
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
-
-
-
-
+  console.log(`app listening on ${host}:${port}`);
+});
