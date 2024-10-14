afa535d134d0970f24390526bc9e88334d67b8c1
noahruppe <noah.ruppe30@gmail.com>
2024-10-14 19:41:13 -0400

    installation of jwt

--- models/account-model.js
+++ models/account-model.js
@@ -31,7 +31,7 @@ async function checkExistingEmail(account_email) {
 * Return account data using email address
 * ***************************** */
 
-async function getAccountByEmail (account_email){
+async function getAccountByEmail (account_email) {
   try {
     const result = await pool.query(
       'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1',
--- server.js
+++ server.js
@@ -51,6 +51,8 @@ app.use(function(req,res,next){
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
+app.use(utilities.checkJWTToken)
+
 
 
 
